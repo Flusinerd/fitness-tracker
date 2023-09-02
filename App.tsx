70aed2fc1da45643bc8cb90a0c20b881b4d5bd47
Jan Krueger <jankrueger1999@gmail.com>
2023-09-02 22:06:40 +0200

    expo initial commit

--- App.tsx
+++ App.tsx
@@ -1,7 +1,33 @@
+import { Accelerometer } from 'expo-sensors';
+import { Subscription } from 'expo-sensors/build/Pedometer';
 import { StatusBar } from 'expo-status-bar';
+import { useEffect, useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const [{x, y, z}, setData] = useState({x: 0, y: 0, z: 0});
+  const [subscription, setSubscription] = useState<Subscription | null>(null);
+  
+  const _fast = Accelerometer.setUpdateInterval(16);
+
+  const _subscribe = () => {
+    setSubscription(Accelerometer.addListener(setData));    
+  }
+
+  const _unsubscribe = () => {
+    subscription && subscription.remove();
+    setSubscription(null);
+  }
+
+  useEffect(() => {
+    _subscribe();
+    return () => _unsubscribe();
+  }, []);
+
+  useEffect(() => {
+    console.log(x, y, z);
+  }, [x, y, z])
+
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
